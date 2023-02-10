45603860e302ebbaf8bb444231bdef132a656256
mohanadkandil <mohanadmkandil@gmail.com>
2023-02-10 07:12:21 -0800

    chore: add folder structure with auth, dashboard views, and some simple UI for testing

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import GlassPane from "@/components/GlassPane";
+import "@/styles/globals.css";
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
