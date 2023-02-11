25ac5a3abb3f45272fe8a9fa6ef8c316df3fef26
mohanadkandil <mohanadmkandil@gmail.com>
2023-02-11 06:10:32 -0800

    feat: add register api

---
+++ pages/api/register.ts
@@ -0,0 +1,35 @@
+import { createJWT, hashPassword } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextApiRequest, NextApiResponse } from "next";
+import { serialize } from "cookie";
+
+export default async function register(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const user = await db.user.create({
+      data: {
+        email: req.body.email,
+        password: await hashPassword(req.body.password),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+    });
+
+    // create a JWT token (object) from the utility method
+    const jwt = await createJWT(user);
+    // save the JWT token in a cookie
+    res.setHeader(
+      "Set-Cookie",
+      serialize(process.env.COOKIE_NAME, jwt, {
+        httpOnly: true,
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+      })
+    );
+
+    res.status(201);
+    res.end();
+  }
+}
